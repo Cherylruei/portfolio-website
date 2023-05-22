c0be416b5e34440667e897f0097d2334f23b6b92
cherylruei <cherylrueichun@gmail.com>
2023-05-22 08:54:07 +0800

    style: about component responsive design

--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -1,5 +1,11 @@
 const Experiences = () => {
-  return <section id='experiences'>Experiences</section>;
+  return (
+    <section id='experiences'>
+      <h2>What skills I have</h2>
+      <h5>My Experiences</h5>
+      <div className='container'>Hello</div>
+    </section>
+  );
 };
 
 export default Experiences;
