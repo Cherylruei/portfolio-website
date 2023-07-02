fb0773e6fb5c43d8bd6fdd87b088426d41a235b0
cherylruei <cherylrueichun@gmail.com>
2023-07-02 12:12:54 +0800

    style: adjust experiences section styles

--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -6,73 +6,77 @@ const Experiences = () => {
       <h2>What skills I have</h2>
       <h5>My Experiences</h5>
       <div className='part'>
-        <div>
+        <div className='frontEndSkills'>
           <h3 className='title'>Front-End</h3>
-          <div className='frontendSkills'>
+          <div className='frontend'>
             <article className='skill'>
-              <RiMedalLine />
-              <h4>HTML</h4>
+              <RiMedalLine className='medal' />
+              <h4>熟悉 HTML 語法</h4>
             </article>
             <article className='skill'>
-              <RiMedalLine />
-              <h4>CSS</h4>
+              <RiMedalLine className='medal' />
+              <h4>熟悉 CSS 排版和 RWD 設計 </h4>
             </article>
             <article className='skill'>
-              <RiMedalLine />
-              <h4>JavaScript</h4>
+              <RiMedalLine className='medal' />
+              <h4>熟悉 JavaScript 技術</h4>
             </article>
             <article className='skill'>
-              <RiMedalLine />
-              <h4>Bootstrap</h4>
+              <RiMedalLine className='medal' />
+              <h4>熟悉 React 及相關開發經驗 </h4>
             </article>
             <article className='skill'>
-              <RiMedalLine />
-              <h4>React</h4>
+              <RiMedalLine className='medal' />
+              <h4>使用 SCSS 預處理器加速開發</h4>
             </article>
             <article className='skill'>
-              <RiMedalLine />
-              <h4>SCSS</h4>
+              <RiMedalLine className='medal' />
+              <h4>後端協作經驗串接 API </h4>
             </article>
             <article className='skill'>
-              <RiMedalLine />
-              <h4>Axios 串接後端API</h4>
+              <RiMedalLine className='medal' />
+              <h4>使用第三方 API 串接的經驗</h4>
             </article>
           </div>
         </div>
-        <div className='otherSkills'>
-          <h3 className='title'>Back-End</h3>
-          <div className='backend'>
-            <article className='skill'>
-              <RiMedalLine />
-              <h4>Node.js / Express</h4>
-            </article>
-            <article className='skill'>
-              <RiMedalLine />
-              <h4>MongoDB 使用經驗</h4>
-            </article>
-            <article className='skill'>
-              <RiMedalLine />
-              <h4>MySQL 基礎概念</h4>
-            </article>
+        <div>
+          <div className='backendSkills'>
+            <h3 className='title'>Back-End</h3>
+            <div className='backend'>
+              <article className='skill'>
+                <RiMedalLine className='medal' />
+                <h4>Node.js / Express 開發經驗 </h4>
+              </article>
+              <article className='skill'>
+                <RiMedalLine className='medal' />
+                <h4>MongoDB 使用經驗</h4>
+              </article>
+              <article className='skill'>
+                <RiMedalLine className='medal' />
+                <h4>MySQL 基礎概念</h4>
+              </article>
+            </div>
           </div>
-          <h3 className='title'>Others</h3>
-          <div className='others'>
-            <article className='skill'>
-              <RiMedalLine />
-              <h4>Git 版本控制</h4>
-            </article>
-            <article className='skill'>
-              <RiMedalLine />
-              <h4>Git 協作經驗</h4>
-            </article>
-            <article className='skill'>
-              <RiMedalLine />
-              <h4>敏捷開發經驗</h4>
-            </article>
-            <article className='skill'>
-              <RiMedalLine />
-              <h4>Notion 專案開發經驗</h4>
-            </article>
+          <div className='otherSkills'>
+            <h3 className='title'>Others</h3>
+            <div className='others'>
+              <article className='skill'>
+                <RiMedalLine className='medal' />
+                <h4>Git 版本控制</h4>
+              </article>
+              <article className='skill'>
+                <RiMedalLine className='medal' />
+                <h4>Git 協作經驗</h4>
+              </article>
+              <article className='skill'>
+                <RiMedalLine className='medal' />
+                <h4>敏捷開發經驗</h4>
+              </article>
+              <article className='skill'>
+                <RiMedalLine className='medal' />
+                <h4>Notion 專案開發經驗</h4>
+              </article>
+            </div>
           </div>
         </div>
       </div>
